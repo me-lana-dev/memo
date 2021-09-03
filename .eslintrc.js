8df649dde675979301eed4ac7ee0aad97c048435
Lana Kaliush <me.lana.dev@gmail.com>
2021-09-03 17:48:57 +0300

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'functional'],
+  rules: {
+    // General
+    'no-console': 'error',
+    // React
+    'react/jsx-boolean-value': 'warn',
+    'react/jsx-curly-brace-presence': 'warn',
+    'react/jsx-fragments': 'warn',
+    'react/jsx-no-useless-fragment': 'warn',
+    'react/jsx-uses-react': 'off',
+    'react/prefer-stateless-function': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+
+    'object-curly-newline': 'off',
+
+    // Functional
+    'functional/prefer-readonly-type': [
+      'warn',
+      {
+        allowLocalMutation: true,
+        allowMutableReturnType: true,
+        ignoreClass: true,
+      },
+    ],
+  },
+};
